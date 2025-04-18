ec490286c7a8506eaa66272bce3a2234fad525b4
sergey.blagodatskikh <sergey.blagodatskikh@x5.ru>
2025-04-18 10:36:57 +0300

    Everything works

--- app/src/main/java/com/example/greetingcard/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/greetingcard/ui/screen/MainScreen.kt
@@ -1,20 +1,38 @@
 package com.example.greetingcard.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
@@ -28,6 +46,12 @@ fun MainScreen(navController: NavController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            var header by remember { mutableStateOf("") }
+            NamePicker(header, List(50) { "item $it" }) { header = it }
+            Box(modifier = Modifier.padding(bottom = 24.dp)) {
+                var clicks by remember { mutableStateOf(0) }
+                OnClicker(clicks) { clicks++ }
+            }
             Text(text = "Firebase Project\nEmail/Password Authentication")
             Row(
                 Modifier
@@ -46,4 +70,46 @@ fun MainScreen(navController: NavController) {
             }
         }
     }
+}
+
+@Composable
+fun OnClicker(clicks: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("I've been clicked ${clicks} times")
+    }
+}
+
+@Composable
+fun NamePicker(
+    header: String,
+    names: List<String>,
+    onNameClicked: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(top=8.dp, bottom = 16.dp).fillMaxHeight(.5f)
+    ) {
+
+        // this will recompose when [header] changes, but not when [names] changes
+        Text(header, fontSize = 34.sp, modifier = Modifier.padding(bottom = 20.dp).fillMaxWidth(),
+            textAlign = TextAlign.Center)
+        HorizontalDivider()
+
+        // LazyColumn is the Compose version of a RecyclerView.
+        // The lambda passed to items() is similar to a RecyclerView.ViewHolder.
+        LazyColumn {
+            items(names) { name ->
+                // When an item's [name] updates, the adapter for that item
+                // will recompose. This will not recompose when [header] changes
+                NamePickerItem(name, onNameClicked)
+            }
+        }
+    }
+}
+
+/**
+ * Display a single name the user can click.
+ */
+@Composable
+private fun NamePickerItem(name: String, onClicked: (String) -> Unit) {
+    Text(name, Modifier.clickable(onClick = { onClicked(name) }))
 }
